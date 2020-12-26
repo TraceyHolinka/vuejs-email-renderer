519cd5b7f7eaf26c8223de6cf8d7e585cec50869
TraceyHolinka <tracey.holinka@gmail.com>
2020-12-26 18:32:52 -0500

    check in

--- src/components/article.js
+++ src/components/article.js
@@ -5,7 +5,7 @@ const article = {
   },
 
   template: `
-    <mj-image v-if="!!article && article.image && article.image.display && article.image.url" width="580px" padding="0" :src="article.image.url" :alt="article.image.alt" />
+    <mj-image v-if="article.image?.display && article.image?.url" width="580px" padding="0" :src="article.image.url" :alt="article.image.alt" />
     <mj-text padding="16px 0 0" color="#0b0b0b">
       <h2 class="headline"><a :href="article.permalink">{{ article.headline }}</a></h2>
       <p class="body">{{ article.summary }} <a :href="article.permalink">Read more...</a></p>
--- src/components/tags.js
+++ src/components/tags.js
@@ -9,7 +9,7 @@ const tags = {
       <mj-column>
         <mj-text font-size="15px" line-height="23px" color="#0b0b0b">
           <span>Learn more about: </span>
-          <span v-for="(tag, index) in tags" :key="index" class="tag"><a :href="tag.id">{{ tag.name }}</a><span v-if="index < tags.length - 1">, </span></span>
+          <span v-for="(tag, index) in tags" :key="tag.id" class="tag"><a :href="tag.id">{{ tag.name }}</a><span v-if="index < tags.length - 1">, </span></span>
         </mj-text>
       </mj-column>
     </mj-section>

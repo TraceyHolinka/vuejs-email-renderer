327b1b93abdc4be97d7561b9f2365cc32fd0218b
TraceyHolinka <tracey.holinka@gmail.com>
2020-12-26 19:24:48 -0500

    Upgrade to Vue 3.

--- src/components/featuredSection.js
+++ src/components/featuredSection.js
@@ -10,7 +10,7 @@ const featuredSection = {
         <mj-text color="#f5f8f7">
           <h1 class="section-title">{{ section.headline }}</h1>
         </mj-text>
-        <mj-image v-if="section.image?.url" width="580px" padding="16px 0 0" border-radius="15px" :src="section.image.url" :alt="section.image.alt" />
+        <mj-image v-if="!!section.image?.url" width="580px" padding="16px 0 0" border-radius="15px" :src="section.image.url" :alt="section.image.alt" />
         <mj-text color="#f5f8f7">
           <div class="body" v-html="section.body" />
         </mj-text>

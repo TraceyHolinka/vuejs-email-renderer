72d70735fe47c17a2afac6e06acff94ec8895eec
TraceyHolinka <tracey.holinka@gmail.com>
2021-03-30 19:41:14 -0400

    clean up css

--- src/assets/css.js
+++ src/assets/css.js
@@ -1,40 +1,6 @@
-// Hover states are generally not supported so leaving links underline is a visual cue that the text is clickable.
+const { readFileSync } = require('fs')
+const { join } = require('path')
 
-exports.inlineCSS = `
-  a {
-    color: #0073ff;
-  }
-  h1 a,
-  h2 a,
-  h3 a {
-    color: #292e31;
-  }
-  img {
-    max-width: 100%;
-  }
-  .section-title {
-    font-size: 26px;
-  }
-  .summary h2,
-  .headline {
-    font-size: 20px;
-  }
-  .body,
-  .summary {
-    font-size: 16px;
-    line-height: 24px;
-  }
-  .footer a {
-    color: #fff;
-  }
-`
-exports.headCSS = `
-  .footer {
-    text-align: center;
-  }
-  @media(min-width: 480px) {
-    .footer {
-      text-align: left;
-    }
-  }
-`
+exports.inlineCSS = readFileSync(join(__dirname, 'inline.css'))
+exports.headCSS = readFileSync(join(__dirname, 'head.css'))
+ 
